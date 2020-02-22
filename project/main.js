1e24ec8ffa2926f01cf490a62eb6fb21dd058485
Joshua Cox <jnokoi@gmail.com>
2020-02-21 22:05:39 -0800

    created basic trivia page

--- project/main.js
+++ project/main.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
+const app = express();
+
+let questions = [];
+let attempts = 0;
+let score = 0;
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -9,14 +17,39 @@ app.set('view engine', 'pug');
 app.set('port', process.argv[2]);
 app.use(express.static('static'));
 
+
 app.get('/', function(req, res) {
   res.render('index', { title: 'puggy!', message: 'taste like pug!'});
 });
 
 
 app.get('/trivia', function(req, res) {
-  res.render('trivia')
-})
+  res.render('trivia', { title: 'trivia' });
+});
+
+
+/*
+  Get next trivia question
+ */
+app.get('/next', function(req, res, next) {
+  getQuestion().then(question => {
+    res.json({ attempts, score, ...questions.pop() });
+  })
+});
+
+
+/*
+  Submit an answer
+ */
+app.post('/answer', function(req, res) {
+  const correct = req.body['correct'];
+  if (correct) {
+    score++;
+  }
+  attempts++;
+
+  res.json({correct})
+});
 
 
 app.use(function(req,res) {
@@ -36,3 +69,19 @@ app.use(function(err, req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Server started on port' + app.get('port'));
 });
+
+
+function getQuestion() {
+  return new Promise((resolve, reject) => {
+    // check if there are enough questions, otherwise get more
+    if (questions.length == 0) {
+      const url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
+      axios.get(url).then(res => {
+        questions = res.data.results;
+        resolve(questions.pop());
+      })
+    } else {
+      resolve(questions.pop());
+    }
+  })
+}
--- project/static/js/script.js
+++ project/static/js/script.js
@@ -1,1 +1,115 @@
-console.log('js loaded');
+
+/*
+  Second answer to server
+ */
+function onAnswer(e) {
+  const { answer, correct } = e.target.dataset;
+  console.log(answer + ' answered');
+  postRequest('http://localhost:3000/answer', {correct: correct == answer})
+    .then(res => {
+      showAnswer(answer);
+  });
+}
+
+/*
+  Setup new question and answers
+ */
+function newRound() {
+  resetAnswer();
+  setQuestions();
+}
+
+
+/*
+  Get new question from server and assign
+ */
+function setQuestions() {
+  getRequest('http://localhost:3000/next').then(res => {
+    // set recent score
+    const scoreElem = document.getElementById('score');
+    scoreElem.innerHTML = `Score: ${res.score}/${res.attempts}`;
+
+    // assign question
+    const questionElem = document.getElementById('question');
+    questionElem.innerHTML = res.question;
+
+    // assign answers
+    const answers = [res.correct_answer].concat(res.incorrect_answers);
+    answers.sort();
+    const answerBtns = document.getElementsByClassName('answer');
+    for (let i = 0 ; i < 4 ; i++) {
+      answerBtns[i].dataset.correct = res.correct_answer;
+      answerBtns[i].dataset.answer = answers[i];
+      answerBtns[i].innerHTML = answers[i];
+      answerBtns[i].onclick = onAnswer;
+    }
+  });
+}
+
+
+/*
+  Highlist correct answer
+ */
+function showAnswer(guess) {
+  const answerBtns = document.getElementsByClassName('answer');
+  for (let btn of answerBtns) {
+    const { answer, correct } = btn.dataset;
+    if (answer == guess && answer != correct) {
+      btn.classList.add('wrong');
+    }
+
+    if (answer == correct) {
+      btn.classList.add('correct');
+    }
+  }
+}
+
+
+/*
+  Reset answer buttons
+ */
+function resetAnswer() {
+  const answerBtns = document.getElementsByClassName('answer');
+  for (let btn of answerBtns) {
+    btn.classList.remove('correct');
+    btn.classList.remove('wrong');
+  }
+}
+
+
+function getRequest(url) {
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open('get', url, true);
+    req.addEventListener('load', () => {
+      let res = JSON.parse(req.responseText);
+      resolve(res);
+    });
+    req.send(null);
+  })
+}
+
+
+function postRequest(url, payload) {
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open('POST', url, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', () => {
+      let res = JSON.parse(req.responseText);
+      resolve(res);
+    })
+    req.send(JSON.stringify(payload));
+  })
+}
+
+
+function main() {
+  const nextBtn = document.getElementById('next');
+  nextBtn.onclick = newRound;
+
+  newRound();
+}
+
+
+document.addEventListener('DOMContentLoaded', main);
