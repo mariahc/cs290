a9ce968aabfffa9fd792f717c5d894aa99216e60
Joshua Cox <jnokoi@gmail.com>
2020-02-27 02:13:56 -0800

    added chat page

--- horse/index.js
+++ horse/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
 const path = require('path');
+const session = require('express-session');
 
 const profiles = require('./profiles.json').profiles;
 
@@ -15,6 +16,20 @@ let score = 0;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({
+  secret: 'secret key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// initialize session
+app.use(function(req, res, next) {
+  if (!req.session.chat) {
+    req.session.chat = {};
+  }
+
+  next();
+})
 
 app.set('view engine', 'pug');
 app.locals.basedir = path.join(__dirname, 'views');
@@ -23,9 +38,9 @@ app.use(express.static('static'));
 
 // disable cache
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
-  next()
-})
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  next();
+});
 
 
 app.get('/', function(req, res) {
@@ -34,18 +49,70 @@ app.get('/', function(req, res) {
 
 
 app.get('/chat', function(req, res) {
-  res.render('chat', { title: 'chat', js: 'chat'});
-})
+  const { user } = req.query;
+
+  if (!user) {
+    // redirect to a random user
+    const randIndex = Math.floor(Math.random() * profiles.length);
+    const randProfile = profiles[randIndex];
+    res.redirect(`/chat?user=${randProfile.name}`);
+  } else {
+    // try to get the user's chat logs
+    const profile = profiles.find( p => p.name === user );
+    if (profile) {
+      if (req.session.chat[user]) {
+        res.render('chat', {
+          title: 'chat',
+          js: 'chat',
+          user: user,
+          messages: req.session.chat[user]
+        });
+
+      } else {
+        req.session.chat[user] = [];
+        res.render('chat', {
+          title: 'chat',
+          js: 'chat',
+          user: user,
+          messages: req.session.chat[user]
+        });
+      }
+    } else {
+      res.sendStatus(404);
+    }
+  }
+});
 
 
 app.get('/browse', function(req, res) {
   res.render('browse', { title: 'browse', profiles });
-})
+});
 
 
 app.get('/profile', function(req, res) {
-  res.render('profile', { title: 'profile' });
-})
+  const { user } = req.query;
+
+  if (!user) {
+    // go to a random user
+    const randIndex = Math.floor(Math.random() * profiles.length);
+    const randProfile = profiles[randIndex];
+    res.redirect(`/profile?user=${randProfile.name}`);
+  } else {
+    const profile = profiles.find( p => p.name == user );
+    if (profile) {
+      res.render('profile', { title: 'profile', profile });
+    } else {
+      res.sendStatus(404);
+    }
+  }
+});
+
+
+app.post('/send', function(req, res) {
+  const { text, user, sender } = req.body;
+  req.session.chat[user].push({ sender, text });
+  res.json({ chat: req.session.chat[user] });
+});
 
 
 // 404
--- horse/static/js/chat.js
+++ horse/static/js/chat.js
@@ -1,6 +1,83 @@
+function send(e) {
+  e.preventDefault();
+
+  const form = document.getElementById('form');
+  if (form.message.value == '') {
+    return;
+  }
+
+  postRequest('/send', {
+    sender: 'sent',
+    user: form.dataset.user,
+    text: form.message.value
+  }).then(res => {
+    showMessages(res.chat);
+  });
+
+  form.message.value = '';
+
+  setTimeout(response, Math.floor(Math.random()*4000));
+}
+
+
+function response() {
+  const form = document.getElementById('form');
+
+  const messages = [
+    'neigh',
+    '*aggressive snorting*',
+    '*whinny*'
+  ];
+  const randMessage = messages[Math.floor(Math.random()*messages.length)];
+
+  postRequest('/send', {
+    sender: 'got',
+    user: form.dataset.user,
+    text: randMessage
+  }).then(res => {
+    showMessages(res.chat);
+  });
+}
+
+
+function showMessages(messages) {
+  const chatbox = document.getElementById('chatbox');
+
+  console.log(messages);
+  // append new messages
+  for (let i = chatbox.children.length ; i < messages.length ; i++) {
+    const message = messages[i];
+    const msgElem = document.createElement('div');
+    const textElem = document.createElement('span');
+    msgElem.classList.add('message');
+    msgElem.classList.add(message.sender);
+    textElem.textContent = message.text;
+    msgElem.appendChild(textElem);
+    chatbox.appendChild(msgElem);
+  }
+
+  chatbox.scrollBy({top: chatbox.scrollHeight, behavior: 'smooth'});
+}
+
+
+function postRequest(url, payload) {
+  return new Promise((resolve, reject) => {
+    let req = new XMLHttpRequest();
+    req.open('POST', url, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', () => {
+      let res = JSON.parse(req.responseText);
+      resolve(res);
+    })
+    req.send(JSON.stringify(payload));
+  })
+}
+
 
 function main() {
-  console.log('chat!');
+  const form = document.getElementById('form');
+  form.addEventListener('submit', send);
+
 }
 
 
